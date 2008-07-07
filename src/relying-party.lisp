a7e7a4d108f8cb9a9bf52b9c2fd6f561424a07fa
Maciek Pasternacki maciej at pasternacki.net <maciej@pasternacki.net>
2008-07-07 18:43:31 +0300

    Rework relying party: unique URLs for each auth process, *IDS* is a hash table now, a bit of HTML cosmetics.
    darcs-hash:20080707154331-08765-29cc9d1e008c68c451a45b3e03929f462fe0345e

--- src/relying-party.lisp
+++ src/relying-party.lisp
@@ -18,62 +18,79 @@
 <input type=\"submit\" name=\"openid_action\" value=\"Login\"></form>")
       "Input form for OpenID, for parameterless indirect method endpoint call.")))
 
-(defvar *ids* ()
+(defvar *ids* (make-hash-table)
   "List of handled IDs")
 
-(defvar *ret-params* ()
-  "List of received indirect response parameters (for debugging purposes)")
+(defpackage :cl-openid.ids
+  (:use)
+  (:documentation "Package for unique keys to *IDS* hashtable."))
 
-(defun handle-openid-request (uri realm parameters)
+(defun initiate-authorization (given-id uri realm
+                               &key immediate-p
+                               &aux
+                               (id (discover (normalize-identifier given-id)))
+                               (handle (gentemp "ID" (find-package :cl-openid.ids)))
+                               (return-to (merge-uris (symbol-name handle) uri)))
+  "Initiate authorization process.  Returns URI to redirect user to."
+  (push (cons :timestamp (get-universal-time)) id)
+  (push (cons :return-to return-to) id)
+  (setf (gethash handle *ids*) id)
+  (request-authentication-uri id
+                              :immediate-p immediate-p
+                              :realm realm
+                              :return-to return-to))
+
+(defun handle-openid-request (uri realm parameters postfix)
   "Handle single OpenID Relying Party request for the received PARAMETERS alist, using URI as return_to address and REALM as a realm.
 
 Returns a string with HTML reply and a redirect URI if applicable."
-  (cond ((null parameters)
-         +openid-input-form+)
-
-        ((aget "openid_identifier" parameters)
-         (let ((id (discover (normalize-identifier (hunchentoot:get-parameter "openid_identifier")))))
-           (push id *ids*)
-           (values nil (request-authentication-uri id :realm realm :return-to uri))))
-
-        (t (push parameters *ret-params*)
-           (format nil
-                   "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"
-   \"http://www.w3.org/TR/html4/strict.dtd\">
-<html><head><title>CL-OpenID result</title></head>
-<body>~A
-<p>realm: ~A</p>
+  (if (null postfix)
+      (if (aget "openid_identifier" parameters)
+          (values nil (initiate-authorization (aget "openid_identifier" parameters) uri realm))
+          +openid-input-form+)
+      (html "CL-OpenID result"
+            "~A <p><strong>realm:</strong> ~A</p>
+<h2>Reply parameters:</h2>
 <dl>~:{<dt>~A</dt><dd>~A</dd>~}</dl>
-<a href=\"~A\">return</a>
-</body></html>"
-                   (let ((reply (handle-indirect-reply parameters (first *ids*) uri)))  ; FIXME: (first *ids*)
-                     (case reply
-                       (:setup-needed "<h1 style=\"color: orange; text-decoration: blink;\">SETUP NEEDED !!!</h1>")
-                       ((nil) "<h1 style=\"color: red; text-decoration: blink;\">ACCESS DENIED !!!</h1>")
-                       (t (format nil
-                                  "<h1 style=\"color: green; text-decoration: blink;\">ACCESS GRANTED !!!</h1><p>ID: ~A</p>"
-                                  reply))))
-                   realm
-                   (mapcar #'(lambda (c)
-                               (list (car c) (cdr c)))
-                           parameters)
-                   uri))))
+<p style=\"text-align:right;\"><a href=\"~A\">return</a><p>"
+            (let ((id (handle-indirect-reply parameters (gethash (intern postfix :cl-openid.ids) *ids*))))
+              (if id
+                  (format nil
+                          "<h1 style=\"color: green; text-decoration: blink;\">ACCESS GRANTED !!!</h1><p>ID: <code>~A</code></p>"
+                          (hunchentoot:escape-for-html (prin1-to-string id))) ; FIXME:hunchentoot
+                  "<h1 style=\"color: red; text-decoration: blink;\">ACCESS DENIED !!!</h1>"))
+            realm
+            (mapcar #'(lambda (c)
+                        (list (car c) (cdr c)))
+                    parameters)
+            uri)))
 
 ;; Hunchentoot-specific part
-(defun openid-ht-handler (uri realm)
+(defun openid-ht-handler (uri realm prefix)
   "Return a Hunchentoot handler for OpenID request for URI and REALM (closure over HANDLE-OPENID-REQUEST)."
   (let ((u (uri uri))
-        (r (uri realm)))
+        (r (uri realm))
+        (l (length prefix)))
     #'(lambda ()
         (multiple-value-bind (content uri)
-            (handle-openid-request u r (hunchentoot:get-parameters))
+            (handle-openid-request u r (hunchentoot:get-parameters)
+                                   ;; Postfix
+                                   (when (> (length (hunchentoot:script-name)) l)
+                                     (subseq (hunchentoot:script-name) l)))
           (when uri
             (hunchentoot:redirect (princ-to-string uri)))
           content))))
 
-(defun openid-ht-dispatcher (prefix realm &optional (uri (merge-uris prefix (uri realm))))
-  "Return a prefix dispatcher for OPENID-HT-HANDLER"
-  (hunchentoot:create-prefix-dispatcher prefix (openid-ht-handler uri realm)))
+(defun openid-ht-dispatcher (prefix realm &optional uri)
+  "Return a prefix dispatcher for OPENID-HT-HANDLER."
+  ;; Ensure trailing slash.
+  (unless (eql #\/ (aref prefix (1- (length prefix))))
+    (setf prefix (concatenate 'string prefix "/")))
+
+  (unless uri
+    (setf uri (merge-uris prefix (uri realm))))
+
+  (hunchentoot:create-prefix-dispatcher prefix (openid-ht-handler uri realm prefix)))
 
 ; (hunchentoot:start-server :port 4242)
 ; (push (openid-ht-dispatcher "/cl-openid" "http://lizard.tasak.gda.pl:4242/") hunchentoot:*dispatch-table*)
