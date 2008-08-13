e82bf81d8b1cf20eba3238e0a0c5d0eac7796765
Maciek Pasternacki <maciej@pasternacki.net>
2008-08-13 16:09:11 +0300

    Export AUTH-PROCESS accessors.
    darcs-hash:20080813130911-6ffcc-fae55d27ce306db06df73535e65084fbac8e3359

--- src/package.lisp
+++ src/package.lisp
@@ -9,6 +9,7 @@
         #:ironclad)
   (:shadowing-import-from :cl #:null) ; Ironclad shadows NULL, we don't want to
   (:export #:relying-party #:realm #:root-uri  ; RP class
+	   #:auth-process #:auth-process-p #:protocol-version-major #:protocol-version-minor #:protocol-version #:claimed-id #:op-local-id #:return-to #:xrds-location #:endpoint-uri #:timestamp ; AUTH-PROCESS structure
            #:initiate-authentication #:+authproc-handle-parameter+ #:handle-indirect-response #:handle-openid-provider-request ; RP API
            #:openid-assertion-error #:code  ; assertion error condition
            #:openid-provider #:op-endpoint-uri  ; OP class
