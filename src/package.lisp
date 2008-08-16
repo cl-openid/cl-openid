d766d21562e6b4da72397be6185e4f3e4da07645
Maciek Pasternacki maciej at pasternacki.net <maciej@pasternacki.net>
2008-08-16 20:21:14 +0300

    Export OPENID-ASSERTION-ERROR condition accessord.
    darcs-hash:20080816172114-08765-571be76cf3074d85d0f4ff9fad78330a8c4b22b2

--- src/package.lisp
+++ src/package.lisp
@@ -12,7 +12,7 @@
            #:relying-party #:realm #:root-uri   ; RP class
 	   #:auth-process #:auth-process-p #:protocol-version-major #:protocol-version-minor #:protocol-version #:claimed-id #:op-local-id #:return-to #:xrds-location #:endpoint-uri #:timestamp ; AUTH-PROCESS structure
            #:initiate-authentication #:+authproc-handle-parameter+ #:handle-indirect-response ; RP API
-           #:openid-assertion-error #:code  ; assertion error condition
+           #:openid-assertion-error #:code #:reason #:authproc #:message ; assertion error condition
            #:openid-provider #:op-endpoint-uri  ; OP class
            #:allow-unencrypted-association-p #:handle-checkid-immediate #:handle-checkid-setup #:checkid-error #:with-checkid-error-handler #:handle-openid-provider-request ; OP API
            #:successful-response #:cancel-response #:+indirect-response-code+ ; responses
